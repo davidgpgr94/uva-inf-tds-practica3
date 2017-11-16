f61d928c5378823dc72a26dbe9b991bf4efc405d
davidgo <david.gonzalez.perez@alumnos.uva.es>
2017-11-16 10:49:03 +0100

    Generado stub del Constructor y los getters

---
+++ src/uva/tds/pr2/equipo09/Coordenada.java
@@ -0,0 +1,19 @@
+package uva.tds.pr2.equipo09;
+
+public class Coordenada {
+
+	public Coordenada(double d, double e) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public double getLatitud() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public double getLongitud() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ srctest/uva/tds/pr2/equipo09/CoordenadaTDDTest.java
@@ -0,0 +1,18 @@
+package uva.tds.pr2.equipo09;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CoordenadaTDDTest {
+
+	public static final double ERROR_ADMISIBLE = 0.0001;
+	
+	@Test
+	public void testTDDConstructorCoordenada() {
+		Coordenada coord = new Coordenada(12.234, -24.356);
+		assertEquals(coord.getLatitud(), 12.234, ERROR_ADMISIBLE);
+		assertEquals(coord.getLongitud(), -24.356, ERROR_ADMISIBLE);
+	}
+
+}

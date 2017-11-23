a11f009ebe28217cf9a33056e818e5fd3fdf5917
ginquin <ginojesus.quintana@alumnos.uva.es>
2017-11-23 10:57:51 +0100

    añadidos test clases no validas

--- srctest/uva/tds/pr2/equipo09/LineaTDDTest.java
+++ srctest/uva/tds/pr2/equipo09/LineaTDDTest.java
@@ -12,9 +12,31 @@ public class LineaTDDTest {
 			Coordenada[] coordenadas={new Coordenada(9.25639,10.25639),new Coordenada(11.25639,12.25639),new Coordenada(12.25639,13.25639)}; 
 			Linea linea = new Linea(identificador,coordenadas);
 			assertEquals(linea.getIdentificador(), 1);
-			assertEquals(coordenadas.length,3);
+			assertEquals(linea.coordenadas.size(),3);
 	}
 	
+	@Test(expected=IllegalArgumentException.class)
+	public void testTDDConstructorLineaIdentificadorNoValido() {
+			int identificador=-1;
+			Coordenada[] coordenadas={new Coordenada(9.25639,10.25639),new Coordenada(11.25639,12.25639),new Coordenada(12.25639,13.25639)}; 
+			Linea linea = new Linea(identificador,coordenadas);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testTDDConstructorLineaCoordenadasNoValidas() {
+			int identificador=1;
+			Coordenada[] coordenadas={new Coordenada(9.25639,10.25639),new Coordenada(11.25639,12.25639)}; 
+			Linea linea = new Linea(identificador,coordenadas);
+			
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testTDDConstructorCoordenadasNull() {
+			int identificador=1;
+			Coordenada[] coordenadas=null; 
+			Linea linea = new Linea(identificador,coordenadas);
+			
+	}
 	
 	
 

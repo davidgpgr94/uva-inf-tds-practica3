c6406f89c9e036448e2f6d43463c9fc50261874a
davidgo <david.gonzalez.perez@alumnos.uva.es>
2017-11-23 09:43:16 +0100

    Contrato del constructor en javadoc de Coordenada

--- src/uva/tds/pr2/equipo09/Coordenada.java
+++ src/uva/tds/pr2/equipo09/Coordenada.java
@@ -2,7 +2,16 @@ package uva.tds.pr2.equipo09;
 
 public class Coordenada {
 
-	public Coordenada(double d, double e) {
+	/**
+	 * Crea un objeto que representa una coordenada en formato GD. 
+	 * Las latitudes Norte son positivas, y las latitudes Sur son negativas.
+	 * Las longitudes Este son positivas, y las longitudes Oeste son negativas.
+	 * @param lat valor para la latitud
+	 * @param lon valor para la longitud
+	 * @post.condition {@code lat = this.getLatitud() }
+	 * @post.condition {@code lon = this.getLongitud() }
+	 */
+	public Coordenada(double lat, double lon) {
 		// TODO Auto-generated constructor stub
 	}
 

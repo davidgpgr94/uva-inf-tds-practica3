522e5db76607b8b8bb0132f8c6d46ee500d271d0
davidgo <david.gonzalez.perez@alumnos.uva.es>
2017-11-28 19:57:40 +0100

    Generando el stub del Constructor y el getLineas de RedAutobuses

--- srctest/uva/tds/pr2/equipo09/RedAutobusesTest.java
+++ srctest/uva/tds/pr2/equipo09/RedAutobusesTest.java
@@ -7,8 +7,12 @@ import org.junit.Test;
 public class RedAutobusesTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testTDDConstructorRedAutobuses() {
+		Coordenada cord1 = new Coordenada(1, 1);
+		Coordenada cord2 = new Coordenada(2, 2);
+		Linea[] lineas = {new Linea(1, cord1), new Linea(2, cord2) };
+		RedAutobuses red = new RedAutobuses(lineas);
+		assertEquals(red.getLineas(), lineas);
 	}
 
 }

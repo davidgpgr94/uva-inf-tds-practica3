e72367c1ab87bb1dc52cea14fc7afad6154ad3a7
davidgo <david.gonzalez.perez@alumnos.uva.es>
2017-11-30 09:56:34 +0100

    Añadido javadoc del constructor

--- src/uva/tds/pr2/equipo09/RedAutobuses.java
+++ src/uva/tds/pr2/equipo09/RedAutobuses.java
@@ -4,6 +4,13 @@ public class RedAutobuses {
 
 	protected Linea[] lineas;
 	
+	/**
+	 * Crea una instancia de RedAutobuses con las líneas pasadas como parámetro.
+	 * 
+	 * @pre.condition {@code lineas.length >= 2}
+	 * @param lineas las líneas de autobús que forman la red de autobuses.
+	 * @throws IllegalArgumentException si {@code lineas.length < 2}
+	 */
 	public RedAutobuses(Linea[] lineas) {
 		// TODO Auto-generated constructor stub
 	}

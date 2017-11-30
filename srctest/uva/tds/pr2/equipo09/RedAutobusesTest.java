d641366f0d17447a6272df9613c20b2c9994f2ec
davidgo <david.gonzalez.perez@alumnos.uva.es>
2017-11-30 10:20:19 +0100

    Añadido test de la excepcion del metodo getLinea

--- srctest/uva/tds/pr2/equipo09/RedAutobusesTest.java
+++ srctest/uva/tds/pr2/equipo09/RedAutobusesTest.java
@@ -32,4 +32,13 @@ public class RedAutobusesTest {
 		
 		assertEquals(new Linea(1, cords1), l1);
 	}
+	
+	@Test(expected = IllegalArgumentException.class) 
+	public void testTDDgetLineaIdConIdNoValido() {
+		Coordenada[] cords1 = { new Coordenada(12.25580, 12.25580), new Coordenada(12.25570, 12.25570), new Coordenada(12.25639, 12.25639) };
+		Coordenada[] cords2 = { new Coordenada(24.3, 24.3), new Coordenada(24.303, 24.303), new Coordenada(24.3006, 24.3006) };
+		Linea[] lineas = { new Linea(1, cords1), new Linea(2, cords2) };
+		RedAutobuses red = new RedAutobuses(lineas);
+		Linea l1 = red.getLinea(0);
+	}
 }

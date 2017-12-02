60a263acdd178920c34c1f3bb0bac20ff6f09a22
ginquin <ginojesus.quintana@alumnos.uva.es>
2017-12-02 17:41:27 +0100

    añadido testSuit con todas las clases de test de Linea

---
+++ src/uva/tds/pr2/equipo09/AllTestsLinea.java
@@ -0,0 +1,14 @@
+package uva.tds.pr2.equipo09;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+	LineaFixtureTDDTest.class,
+	LineaTDDTest.class,
+})
+public class AllTestsLinea {
+
+}

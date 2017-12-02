f8328a9183209216682130d5faddf29bc67185b3
davidgo <david.gonzalez.perez@alumnos.uva.es>
2017-12-02 16:55:12 +0100

    Creado testSuit con todas las clases de test para la clase RedAutobuses

---
+++ srctest/uva/tds/pr2/equipo09/AllTestsRedAutobuses.java
@@ -0,0 +1,13 @@
+package uva.tds.pr2.equipo09;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ RedAutobusesFixtureRedConTresLineasDosConCorrespondenciaTDDTest.class,
+		RedAutobusesFixtureRedConTresLineasDosConTransbordoDirectoTDDTest.class, RedAutobusesFixtureTDDTest.class,
+		RedAutobusesTDDTest.class })
+public class AllTestsRedAutobuses {
+
+}

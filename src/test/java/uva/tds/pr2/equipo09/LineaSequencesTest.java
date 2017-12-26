10af0743177b676cae4aaffd450468e4b31c49f4
davidgo <david.gonzalez.perez@alumnos.uva.es>
2017-12-26 13:13:02 +0100

    Añadidos nuevos test de secuencia para Linea

--- src/test/java/uva/tds/pr2/equipo09/LineaSequencesTest.java
+++ src/test/java/uva/tds/pr2/equipo09/LineaSequencesTest.java
@@ -10,12 +10,12 @@ import org.junit.experimental.categories.Category;
 public class LineaSequencesTest {
 
 	private Linea linea;
+	private Coordenada p0 = new Coordenada(10, 10);
+	private Coordenada p1 = new Coordenada(10.001, 10.001);
+	private Coordenada p2 = new Coordenada(10.0006, 10.0006);
 	
 	@Before
 	public void setUp() throws Exception {
-		Coordenada p0 = new Coordenada(10, 10);
-		Coordenada p1 = new Coordenada(10.001, 10.001);
-		Coordenada p2 = new Coordenada(10.0006, 10.0006);
 		Coordenada[] paradasIniciales = {p0, p1, p2};
 		
 		linea = new Linea(1, paradasIniciales);
@@ -26,8 +26,57 @@ public class LineaSequencesTest {
 		Coordenada nuevaParada = new Coordenada(10.009, 10.009);
 		linea.añadirParada(1, nuevaParada);
 		linea.eliminarParada(2);
-		Coordenada[] exp = {new Coordenada(10, 10), nuevaParada, new Coordenada(10.0006, 10.0006)};
+		Coordenada[] exp = {p0, nuevaParada, p2};
 		assertArrayEquals(exp, linea.getParadas());
 	}
-
+	
+	@Test
+	public void testSecuenciaAñadirAñadirYEliminarParada() {
+		Coordenada np1 = new Coordenada(10.009, 10.009);
+		Coordenada np2 = new Coordenada(10.01, 10.01);
+		linea.añadirParada(1, np1);
+		linea.añadirParada(2, np2);
+		linea.eliminarParada(3);
+		Coordenada[] exp = {p0, np1, np2, p2};
+		assertArrayEquals(exp, linea.getParadas());
+	}
+	
+	@Test(expected = IllegalStateException.class)
+	public void testSecuenciaAñadirEliminarYEliminarParada() {
+		Coordenada np = new Coordenada(10.009, 10.009);
+		linea.añadirParada(1, np);
+		linea.eliminarParada(2);
+		linea.eliminarParada(1);
+	}
+	
+	@Test
+	public void testSecuenciaAñadirParadaYModificarInicial() {
+		Coordenada np = new Coordenada(10.009, 10.009);
+		Coordenada ni = new Coordenada(10.0001, 10.0001);
+		linea.añadirParada(1, np);
+		linea.cambiarParadaInicial(ni);
+		Coordenada[] exp = {ni, np, p1, p2};
+		assertArrayEquals(exp, linea.getParadas());
+	}
+	
+	@Test
+	public void testSecuenciaAñadirEliminarYModificarFinal() {
+		Coordenada np = new Coordenada(10.009, 10.009);
+		Coordenada nf = new Coordenada(10.0001, 10.0001);
+		linea.añadirParada(1, np);
+		linea.eliminarParada(2);
+		linea.cambiarParadaFinal(nf);
+		Coordenada[] exp = {p0, np, nf};
+		assertArrayEquals(exp, linea.getParadas());
+	}
+	
+	@Test
+	public void testSecuenciaAñadirEliminarYAñadirParada() {
+		Coordenada np = new Coordenada(10.009, 10.009);
+		linea.añadirParada(1, np);
+		linea.eliminarParada(2);
+		linea.añadirParada(1, p1);
+		Coordenada[] exp = {p0, p1, np, p2};
+		assertArrayEquals(exp, linea.getParadas());
+	}
 }
